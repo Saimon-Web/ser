9fa0e3ca885fbcbaa9eead86a2d873740b1ed6e3
naim <naimctg2000@gmail.com>
2022-05-19 13:24:23 +0600

    '

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cors=require('cors');
 const app=express();
-const port =process.env.PORT;
+const port =process.env.PORT || 2178;
 
 require('dotenv').config()
 //Middle ware
